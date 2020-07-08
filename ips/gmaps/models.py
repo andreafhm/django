b3b5e1f45b372a19c97307acee8c2cbb910f4424
alex <jersin2304@gmail.com>
2020-07-08 00:23:33 -0500

    ga

--- ips/gmaps/models.py
+++ ips/gmaps/models.py
@@ -2,23 +2,23 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.forms import ModelForm
 
-class Ubicacion(models.Model):
-	nom_comisaria = models.CharField(max_length=200)
-	lat = models.CharField(max_length=50)
-	lng = models.CharField(max_length=50)
-	nombredd = models.CharField(max_length=50)
-	nombrepp = models.CharField(max_length=50)
-	nombredi = models.CharField(max_length=50)
-	user = models.ForeignKey('auth.User',on_delete=models.CASCADE)
 
-	def __str__(self):
-		return self.nom_comisaria
+class Ubicacion(models.Model):
+    nom_comisaria = models.CharField(max_length=200)
+    lat = models.CharField(max_length=50)
+    lng = models.CharField(max_length=50)
+    nombredd = models.CharField(max_length=50)
+    nombrepp = models.CharField(max_length=50)
+    nombredi = models.CharField(max_length=50)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nom_comisaria
+
 
 class UbicacionForm(ModelForm):
-	class Meta:
-		model = Ubicacion
-		fields = "__all__"
-
-
+    class Meta:
+        model = Ubicacion
+        fields = "__all__"
 
 # Create your models here.
